9a8dcd7bd0d24775da69d9f43f16c5c240c92848
AmanKawadia26 <akawadia25@gmail.com>
2023-08-25 11:55:23 +0530

    second

---
+++ script/input.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let submit = document.getElementById('submit')
+    submit.addEventListener('click', redirectToScore)
+})
+
+const redirectToScore = () => {
+    let team1 = document.getElementById('team1name').value;
+    let team2 = document.getElementById('team2name').value;
+    let overs = (document.getElementById('overs').value)
+    if(team1 != "" && team2 != "" && overs != ""){
+        overs = parseInt(document.getElementById('overs').value)
+        localStorage.setItem('team1name', team1)
+        localStorage.setItem('team2name', team2)
+        localStorage.setItem('maxOver', overs)
+        window.location.replace("main.html")
+    }
+    else{
+        alert("Fill the required values!")
+    }
+}
